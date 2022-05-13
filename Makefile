32d7fd0457e8cba2271d1f4f6164ba4eb54d8355
0pling <git@0pling.com>
2022-05-14 03:27:44 +1000

    use dotenv for setting up env vars

--- Makefile
+++ Makefile
@@ -1,7 +1,15 @@
-init:
+init: .env
 	pip install -r requirements.txt
+	echo "Please put your Twitter API access keys inside the '.env' file."
 
 test:
 	python -m unittest discover -v -s tests
+
+.env:
+	touch ./.env
+	echo "CONSUMER_KEY='your Twitter consumer key'" >> .env
+	echo "CONSUMER_SECRET='your Twitter consumer secret'" >> .env
+	echo "ACCESS_TOKEN='your Twitter access token'" >> .env
+	echo "ACCESS_TOKEN_SECRET='your Twitter access token secret'" >> .env
 
 .PHONY: init test
--- w3igg_tweet/__main__.py
+++ w3igg_tweet/__main__.py
@@ -1,9 +1,12 @@
 import argparse
 import sys
 from selenium import webdriver
+from dotenv import load_dotenv
 from .core import get_entry, tweet
 
+
 if __name__ == "__main__":
+    load_dotenv()
     parser = argparse.ArgumentParser(description="Web3 is great! Just by the dip.")
     parser.add_argument("--url", type=str, help="URL of the entry to tweet")
     args = parser.parse_args()
@@ -17,14 +20,8 @@ if __name__ == "__main__":
         print(f"Tweeted '{title}'.")
     except KeyError:
         print(
-            "Error: Required environment variables are missing. Make sure the following environment variables are set:",
+            "Error: Missing environment variables. Set the Twitter API access keys in .env.",
             file=sys.stderr,
-        )
-        print("- CONSUMER_KEY : your Twitter consumer key", file=sys.stderr)
-        print("- CONSUMER_SECRET : your Twitter consumer secret", file=sys.stderr)
-        print("- ACCESS_TOKEN : your Twitter access token", file=sys.stderr)
-        print(
-            "- ACCESS_TOKEN_SECRET : your Twitter access token secret", file=sys.stderr
         )
     finally:
         driver.quit()
